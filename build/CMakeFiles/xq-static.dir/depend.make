e2752c0ce0a0165cc2404e150f90881b21544537
Mariana Meireles <mariana@psychonautgirl.space>
2021-09-13 20:00:19 +0200

    Rm build dirs

--- build/CMakeFiles/xq-static.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for xq-static.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/xq-static_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/xq-static_autogen"
-  "xq-static_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/xq-static_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
