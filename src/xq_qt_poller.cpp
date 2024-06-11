3dc833846071c625694836ca3eec93a8f4f26ee1
Johan Mabille <johan.mabille@gmail.com>
2024-06-11 06:44:12 +0200

    Upgraded to xeus-zmq 3

--- src/xq_qt_poller.cpp
+++ src/xq_qt_poller.cpp
@@ -1,12 +1,59 @@
+/***************************************************************************
+* Copyright (c) 2021, QuantStack                                           *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include <iostream>
+
 #include "xq/xq_qt_poller.hpp"
 
-#include "xeus-zmq/xserver_zmq.hpp"
-#include "xeus-zmq/xzmq_serializer.hpp"
-#include "xeus-zmq/xmiddleware.hpp"
+namespace xeus
+{
 
-#include <zmq_addon.hpp>
+    WorkerThread::WorkerThread(QObject* parent,
+                               poller_t poller)
+        : QThread(parent)
+        , m_poller(std::move(poller))
+        , m_request_stop(false)
+    {
+        std::cout << "starting thread" << std::endl;
+    }
 
-WorkerThread::WorkerThread(QObject* parent,
+    void WorkerThread::run()
+    {
+        while(!m_request_stop.load())
+        {
+            auto msg = m_poller(10);
+            if (msg)
+            {
+                if (msg.value().second == channel::SHELL)
+                {
+                    // signals do not like the move semantics so
+                    // we need to put in a pointer and delete it on the receiving end
+                    xeus::xmessage* pmsg = new xeus::xmessage(std::move(msg.value().first));
+                    emit received_shell_msg_signal(pmsg);
+                }
+                else
+                {
+                    // signals do not like the move semantics so
+                    // we need to put in a pointer and delete it on the receiving end
+                    xeus::xmessage* pmsg = new xeus::xmessage(std::move(msg.value().first));
+                    emit received_controll_msg_signal(pmsg);
+                }
+            }
+        }
+    }
+
+    void WorkerThread::stop()
+    {
+        m_request_stop = true;
+    }
+}
+
+/*WorkerThread::WorkerThread(QObject* parent,
                            zmq::socket_t* p_shell,
                            zmq::socket_t* p_controller,
                            xeus::xauthentication* auth)
@@ -67,4 +114,4 @@ void WorkerThread::run()
             }
         }
     }
-}
+}*/
--- src/xq_server.cpp
+++ src/xq_server.cpp
@@ -1,4 +1,72 @@
-#include <memory>
+/***************************************************************************
+* Copyright (c) 2021, QuantStack                                           *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include "xq/xq_server.hpp"
+#include "xq/xq_qt_poller.hpp"
+
+namespace xeus
+{
+    xq_server::xq_server(xcontext& context,
+                         const xeus::xconfiguration& c,
+                         nl::json::error_handler_t eh)
+        : xserver_zmq(context, c, eh)
+        , m_worker_thread(this, [this](long timeout) {
+                return poll_channels(timeout); })
+    {
+    }
+
+    void xq_server::start_impl(xpub_message message)
+    {
+        start_publisher_thread();
+        start_heartbeat_thread();
+
+        publish(std::move(message), xeus::channel::SHELL);
+
+        start_poller_qthread();
+    }
+
+    void xq_server::stop_impl()
+    {
+        m_worker_thread.stop();
+        m_worker_thread.wait();
+        this->stop_channels();
+    }
+
+    void xq_server::start_poller_qthread()
+    {
+        connect(&m_worker_thread, &WorkerThread::received_shell_msg_signal, this, &xq_server::on_received_shell_msg);
+        connect(&m_worker_thread, &WorkerThread::received_controll_msg_signal, this, &xq_server::on_received_controll_msg);
+        m_worker_thread.start();
+    }
+
+    void xq_server::on_received_controll_msg(xmessage * pmsg)
+    {
+        xmessage msg(std::move(*pmsg));
+        xserver::notify_control_listener(std::move(msg));
+        delete pmsg;
+    }
+
+    void xq_server::on_received_shell_msg(xmessage * pmsg)
+    {
+        xmessage msg( std::move(*pmsg));
+        xserver::notify_shell_listener(std::move(msg));
+        delete pmsg;
+    }
+
+    std::unique_ptr<xserver> make_xq_server(xcontext& context,
+                                            const xeus::xconfiguration& config,
+                                            nl::json::error_handler_t eh)
+    {
+        return std::make_unique<xq_server>(context, config, eh);
+    }
+}
+
+/*#include <memory>
 #include <thread>
 #include <iostream>
 
@@ -66,4 +134,4 @@ void xq_server::on_received_shell_msg(xeus::xmessage * pmsg)
     xeus::xmessage msg( std::move(*pmsg));
     xeus::xserver::notify_shell_listener(std::move(msg));
     delete pmsg;
-}
+}*/
